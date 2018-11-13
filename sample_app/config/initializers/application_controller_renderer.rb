dca20886f74fcc43be3c99073734aeb11a0ea330
unknown <i1i1.00263@gmail.com>
2018-11-13 21:55:32 +0900

    [Finish]3-10

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '14d73ddc67fdcd39f5f4a5c59236d103953d2b6de0e79c6a75b3d2ff8f96ec72f0103943d06906adb87ed61f2b88918866e82f464ce84fa898d52c037db801dc'
